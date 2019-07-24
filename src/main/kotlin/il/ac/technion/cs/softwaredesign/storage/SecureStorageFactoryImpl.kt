c44b2e152b8c402b422035a9cba1e7bc74023363
Golan, Itay <goitay@campus.technion.ac.il>
2019-07-24 21:58:40 +0300

    HW2 - fixed factory

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorageFactoryImpl.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorageFactoryImpl.kt
@@ -8,12 +8,14 @@ import java.util.HashMap
 class SecureStorageFactoryImpl constructor() : SecureStorageFactory {
     private val storagesMap = HashMap<ByteArrayKey, SecureStorage>()
     private val MILISECONDS_DELAY_PER_DB = 100
+
     override fun open(name: ByteArray): CompletableFuture<SecureStorage> {
         return CompletableFuture.supplyAsync {
             val key = ByteArrayKey(name)
-
-            if (!storagesMap.containsKey(key)) {
-                storagesMap[key] = SecureStorageImpl()
+            synchronized(this) {
+                if (!storagesMap.containsKey(key)) {
+                    storagesMap[key] = SecureStorageImpl()
+                }
             }
             Thread.sleep(MILISECONDS_DELAY_PER_DB * storagesMap.size.toLong())
             storagesMap[key]!!
